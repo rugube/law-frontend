66270a227cfd6a3d863622ad586e15f30c8f1a7a
Ronald Rugube <rrugube69@gmail.com>
2023-08-18 16:35:59 +0200

    added videocall UI Kit

--- src/components/VideoCalls/videocalls.jsx
+++ src/components/VideoCalls/videocalls.jsx
@@ -1,177 +1,81 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import {
-  MeetingProvider,
-  MeetingConsumer,
-  useMeeting,
-  useParticipant,
-} from "@videosdk.live/react-sdk";
-import { authToken, createMeeting } from "./api";
-import ReactPlayer from "react-player";
+import * as React from 'react';
+import './style.css';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
-function JoinScreen({ getMeetingAndToken }) {
-    const [meetingId, setMeetingId] = useState(null);
-    const onClick = async () => {
-      await getMeetingAndToken(meetingId);
-    };
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Enter Meeting Id"
-          onChange={(e) => {
-            setMeetingId(e.target.value);
-          }}
-        />
-        <button onClick={onClick}>Join</button>
-        {" or "}
-        <button onClick={onClick}>Create Meeting</button>
-      </div>
-    );
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
   }
-
-  function ParticipantView(props) {
-    const micRef = useRef(null);
-    const { webcamStream, micStream, webcamOn, micOn, isLocal, displayName } =
-      useParticipant(props.participantId);
-  
-    const videoStream = useMemo(() => {
-      if (webcamOn && webcamStream) {
-        const mediaStream = new MediaStream();
-        mediaStream.addTrack(webcamStream.track);
-        return mediaStream;
-      }
-    }, [webcamStream, webcamOn]);
-  
-    useEffect(() => {
-      if (micRef.current) {
-        if (micOn && micStream) {
-          const mediaStream = new MediaStream();
-          mediaStream.addTrack(micStream.track);
-  
-          micRef.current.srcObject = mediaStream;
-          micRef.current
-            .play()
-            .catch((error) =>
-              console.error("videoElem.current.play() failed", error)
-            );
-        } else {
-          micRef.current.srcObject = null;
-        }
-      }
-    }, [micStream, micOn]);
-  
-    return (
-      <div>
-        <p>
-          Participant: {displayName} | Webcam: {webcamOn ? "ON" : "OFF"} | Mic:{" "}
-          {micOn ? "ON" : "OFF"}
-        </p>
-        <audio ref={micRef} autoPlay playsInline muted={isLocal} />
-        {webcamOn && (
-          <ReactPlayer
-            //
-            playsinline // very very imp prop
-            pip={false}
-            light={false}
-            controls={false}
-            muted={true}
-            playing={true}
-            //
-            url={videoStream}
-            //
-            height={"300px"}
-            width={"300px"}
-            onError={(err) => {
-              console.log(err, "participant video error");
-            }}
-          />
-        )}
-      </div>
-    );
-  }
-
-function Controls() {
-    const { leave, toggleMic, toggleWebcam } = useMeeting();
-    return (
-      <div>
-        <button onClick={() => leave()}>Leave</button>
-        <button onClick={() => toggleMic()}>toggleMic</button>
-        <button onClick={() => toggleWebcam()}>toggleWebcam</button>
-      </div>
-    );
-  }
-
-function MeetingView(props) {
-    const [joined, setJoined] = useState(null);
-    //Get the method which will be used to join the meeting.
-    //We will also get the participants list to display all participants
-    const { join, participants } = useMeeting({
-      //callback for when meeting is joined successfully
-      onMeetingJoined: () => {
-        setJoined("JOINED");
-      },
-      //callback for when meeting is left
-      onMeetingLeft: () => {
-        props.onMeetingLeave();
-      },
-    });
-    const joinMeeting = () => {
-      setJoined("JOINING");
-      join();
-    };
-  
-    return (
-      <div className="container">
-        <h3>Meeting Id: {props.meetingId}</h3>
-        {joined && joined == "JOINED" ? (
-          <div>
-            <Controls />
-            {[...participants.keys()].map((participantId) => (
-              <ParticipantView
-                participantId={participantId}
-                key={participantId}
-              />
-            ))}
-          </div>
-        ) : joined && joined == "JOINING" ? (
-          <p>Joining the meeting...</p>
-        ) : (
-          <button onClick={joinMeeting}>Join</button>
-        )}
-      </div>
-    );
-  }
-
-function VideoCalls() {
-  const [meetingId, setMeetingId] = useState(null);
-
-  //Getting the meeting id by calling the api we just wrote
-  const getMeetingAndToken = async (id) => {
-    const meetingId =
-      id == null ? await createMeeting({ token: authToken }) : id;
-    setMeetingId(meetingId);
-  };
-
-  //This will set Meeting Id to null when meeting is left or ended
-  const onMeetingLeave = () => {
-    setMeetingId(null);
-  };
-
-  return authToken && meetingId ? (
-    <MeetingProvider
-      config={{
-        meetingId,
-        micEnabled: true,
-        webcamEnabled: true,
-        name: "Ronald Rugube",
-      }}
-      token={authToken}
-    >
-      <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
-    </MeetingProvider>
-  ) : (
-    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
-  );
+  return result;
 }
 
-export default VideoCalls;
+// get token
+function generateToken(tokenServerUrl: string, userID: string) {
+  // Obtain the token interface provided by the App Server
+  return fetch(
+    `${tokenServerUrl}/access_token?userID=${userID}&expired_ts=7200`,
+    {
+      method: 'GET',
+    }
+  ).then((res) => res.json());
+}
+
+export function getUrlParams(
+  url: string = window.location.href
+): URLSearchParams {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function App() {
+  const roomID = getUrlParams().get('roomID') || randomID(5);
+  const userID = randomID(5);
+  const userName = randomID(5);
+  let myMeeting = async (element: HTMLDivElement) => {
+    // generate token
+    const token = await generateToken(
+      'https://nextjs-token.vercel.app/api',
+      userID
+    );
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForProduction(
+      1484647939,
+      token,
+      roomID,
+      userID,
+      userName
+    );
+    // create instance object from token
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url:
+            window.location.origin +
+            window.location.pathname +
+            '?roomID=' +
+            roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
+
+  return (
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
+}
