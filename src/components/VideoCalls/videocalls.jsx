6d799dd1fe40e2178b0838cca89ab070de2f5567
Ronald Rugube <rrugube69@gmail.com>
2023-08-18 19:47:38 +0200

    '

--- src/components/VideoCalls/videocalls.jsx
+++ src/components/VideoCalls/videocalls.jsx
@@ -1,7 +1,24 @@
-import * as React from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+// VideoCallPage.js
+import React from 'react';
 import { navigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext/AuthState';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+function randomID(len) {
+  let result = '';
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP';
+  var maxPos = chars.length;
+  len = len || 5;
+  for (var i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
+function getUrlParams(url = window.location.href) {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
 
 function VideoCallPage() {
   const { isAuthenticated } = useAuth();
@@ -11,65 +28,32 @@ function VideoCallPage() {
     return null;
   }
 
+  const roomID = getUrlParams().get('roomID') || randomID(5);
 
+  const myMeeting = async (element) => {
+    const appID = 1675037019;
+    const serverSecret = "7940e8d348ae90d085fc2f132c905080";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, randomID(5), randomID(5));
 
-function randomID(len) {
-  let result = '';
-  if (result) return result;
-  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-    maxPos = chars.length,
-    i;
-  len = len || 5;
-  for (i = 0; i < len; i++) {
-    result += chars.charAt(Math.floor(Math.random() * maxPos));
-  }
-  return result;
-}
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
 
-export function getUrlParams(
-  url = window.location.href
-) {
-  let urlStr = url.split('?')[1];
-  return new URLSearchParams(urlStr);
-}
-
-export default function App() {
-      const roomID = getUrlParams().get('roomID') || randomID(5);
-      let myMeeting = async (element) => {
-     // generate Kit Token
-      const appID = 1675037019;
-      const serverSecret = "7940e8d348ae90d085fc2f132c905080";
-      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
-
-
-     // Create instance object from Kit Token.
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-      // start the call
-      zp.joinRoom({
-        container: element,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url:
-             window.location.protocol + '//' + 
-             window.location.host + window.location.pathname +
-              '?roomID=' +
-              roomID,
-          },
-        ],
-        scenario: {
-          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
         },
-      });
-
-
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
   };
 
   return (
-    <div
-      className="myCallContainer"
-      ref={myMeeting}
-      style={{ width: '100vw', height: '100vh' }}
-    ></div>
+    <div className="myCallContainer" ref={myMeeting} style={{ width: '100vw', height: '100vh' }}></div>
   );
 }
+
+export default VideoCallPage;
