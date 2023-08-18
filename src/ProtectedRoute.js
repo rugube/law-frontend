c84471c77d8ed0d5bce02d3749be48abf0fdd677
Ronald Rugube <rrugube69@gmail.com>
2023-08-18 19:29:46 +0200

    fixed import

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Route, Navigate } from 'react-router-dom';
-import { AuthContext } from "../src/context/AuthContext"; // Update the path accordingly
+import { AuthContext } from "./context/AuthContext"; // Update the path accordingly
 
 const ProtectedRoute = ({ path, element }) => {
   const { Auth } = useContext(AuthContext); // Update with the appropriate context variable
