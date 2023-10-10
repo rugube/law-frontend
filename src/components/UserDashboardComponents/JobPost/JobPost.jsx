3bd903478bef88fb1353ae319d9840117082f4a1
rugube <rrugube69@gmail.com>
2023-10-10 11:28:37 +0200

    job post back-end intergration

--- src/components/UserDashboardComponents/JobPost/JobPost.jsx
+++ src/components/UserDashboardComponents/JobPost/JobPost.jsx
@@ -1,29 +1,38 @@
 import React, { useState } from "react";
-import { Card, Input, Button } from "antd";
+import { Card, Input, Button, Select } from "antd";
+import axios from "axios";
 
 const { TextArea } = Input;
+const { Option } = Select;
 
-const JobPost = ({ UserData, jobs, setJobs, notification }) => {
+const JobPost = ({ UserData, notification }) => {
   const [jobTitle, setJobTitle] = useState("");
+  const [serviceType, setServiceType] = useState("contract review");
   const [jobDescription, setJobDescription] = useState("");
 
-  const postJob = () => {
-    // Create a new job object
-    const newJob = {
-      title: jobTitle,
-      description: jobDescription,
-      postedBy: UserData.id, // Assuming UserData contains user information
-    };
+  const postJob = async () => {
+    try {
+      const response = await axios.post("/api/jobs", {
+        userId: UserData._id, // Assuming UserData contains user information
+        serviceType,
+        description: jobDescription,
+      });
 
-    // Update the jobs state with the new job
-    setJobs([...jobs, newJob]);
-
-    // Show a success notification
-    notification("Job Posted", "Your job has been posted successfully.");
-
-    // Clear input fields
-    setJobTitle("");
-    setJobDescription("");
+      if (response.status === 201) {
+        // Job posting successful
+        notification("Job Posted", "Your job has been posted successfully.");
+        // Clear input fields
+        setJobTitle("");
+        setServiceType("contract review");
+        setJobDescription("");
+      } else {
+        // Job posting failed
+        notification("Error", "Failed to post the job.");
+      }
+    } catch (error) {
+      console.error("Error posting job:", error);
+      notification("Error", "An error occurred while posting the job.");
+    }
   };
 
   return (
@@ -35,6 +44,16 @@ const JobPost = ({ UserData, jobs, setJobs, notification }) => {
           value={jobTitle}
           onChange={(e) => setJobTitle(e.target.value)}
         />
+        <Select
+          value={serviceType}
+          onChange={(value) => setServiceType(value)}
+          style={{ width: "100%", marginBottom: "16px" }}
+        >
+          <Option value="contract review">Contract Review</Option>
+          <Option value="litigation">Litigation</Option>
+          <Option value="legal consultation">Legal Consultation</Option>
+          <Option value="other">Other</Option>
+        </Select>
         <TextArea
           placeholder="Job Description"
           value={jobDescription}
