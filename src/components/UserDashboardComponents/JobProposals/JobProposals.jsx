b921da8694cd44d6c9b8498f3e2101b88e48820d
rugube <rrugube69@gmail.com>
2023-10-12 18:41:42 +0200

    design

--- src/components/UserDashboardComponents/JobProposals/JobProposals.jsx
+++ src/components/UserDashboardComponents/JobProposals/JobProposals.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Modal, Card, Button, List, Typography, Row, Col } from "antd";
+import { Modal, Card, Button, List, Row, Col, Typography } from "antd";
 import HOST from "../../../utils/baseUrl";
 
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
 const JobProposals = () => {
   const [jobs, setJobs] = useState([]);
@@ -52,24 +52,19 @@ const JobProposals = () => {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <Title level={2}>View Proposals</Title>
-      <Row gutter={[16, 16]} justify="center">
+    <div>
+      <h1>View Proposals</h1>
+      <Row gutter={16}>
         {jobs.map((job) => (
-          <Col key={job._id} xs={24} sm={12} md={8} lg={6} xl={6}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={6} key={job._id}>
             <Card
-              title={<Text strong>{job.title}</Text>}
+              title={job.title}
               extra={
                 <Button onClick={() => handleViewProposals(job)}>View Proposals</Button>
               }
-              style={{ height: "100%" }}
             >
-              <p>
-                <Text strong>Service Type:</Text> {job.serviceType}
-              </p>
-              <p>
-                <Text strong>Description:</Text> {job.description}
-              </p>
+              <p>Service Type: {job.serviceType}</p>
+              <p>Description: {job.description}</p>
             </Card>
           </Col>
         ))}
@@ -86,7 +81,7 @@ const JobProposals = () => {
             dataSource={selectedJob.proposals}
             renderItem={(proposal) => (
               <List.Item>
-                <Card title={<Text strong>{`Lawyer: ${proposal.lawyerId}`}</Text>}> {/* Fixed the typo here */}
+                <Card title={<Text strong>{`Lawyer: ${proposal.lawyerId}`}</Text>}>
                   <p>{proposal.proposal}</p>
                   <Button
                     type="primary"
