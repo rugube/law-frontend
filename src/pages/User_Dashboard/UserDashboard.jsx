ecd2ab48a6cef2c01c2b6bb73b63a28105f30724
rugube <rrugube69@gmail.com>
2023-10-16 18:24:09 +0200

    added cards

--- src/pages/User_Dashboard/UserDashboard.jsx
+++ src/pages/User_Dashboard/UserDashboard.jsx
@@ -5,7 +5,7 @@ import AppointmentsArea from "../../components/UserDashboardComponents/Appointme
 import HOST from "../../utils/baseUrl";
 import { UserContext } from "../../context/Admin_page/userFunction/userState";
 import { useNavigate } from "react-router-dom";
-import { notification } from "antd";
+import { notification, Card, Statistic, Row, Col } from "antd";
 import { AuthContext } from "../../context/AuthContext/AuthState";
 import Loading from "../../components/AdminCompo/Loading";
 
@@ -67,10 +67,29 @@ const UserDashboard = () => {
   return !Auth ? (
     <Loading />
   ) : (
-    <div>
-      {contextHolder}
+    <div style={{ padding: "20px" }}>
       <DashNavbar UserData={UserData} />
       <UserProfile UserData={UserData} />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Card title="Upcoming Meetings" style={{ width: "30%", height: "200px" }}>
+          <p>Meeting 1</p>
+          <p>Meeting 2</p>
+          <p>Meeting 3</p>
+        </Card>
+        <Card title="Total Amount Spent" style={{ width: "30%", height: "200px" }}>
+          <Statistic
+            title="Total Amount Spent"
+            value={1000} // Replace with actual total amount
+            prefix="$"
+          />
+        </Card>
+        <Card title="Jobs Completed" style={{ width: "30%", height: "200px" }}>
+          <Statistic
+            title="Jobs Completed"
+            value={50} // Replace with actual total completed jobs
+          />
+        </Card>
+      </div>
       <AppointmentsArea
         notification={openNotification}
         fnotification={FopenNotification}
