097ed04331dafeb56d905b6fff05caf96e789f52
rugube <rrugube69@gmail.com>
2023-10-20 09:37:16 +0200

    all jobs redesign

--- src/pages/User_Dashboard/JobsPage.jsx
+++ src/pages/User_Dashboard/JobsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Tabs, Card, Row, Col } from 'antd';
+import { Tabs, Card, Row, Col, Button } from 'antd';
 import DashNavbar from '../../components/UserDashboardComponents/DashNavbar/DashNavbar';
 import UserProfile from '../../components/UserDashboardComponents/UserProfile/UserProfile';
 
@@ -16,28 +16,23 @@ const JobsPage = () => {
     <div className="jobs-page">
       <DashNavbar />
       <UserProfile />
-      <div className="jobs-container">
+      <div className="jobs-container" style={{ padding: '20px' }}>
         <h1 style={{ textAlign: 'center' }}>Jobs</h1>
-        <Tabs
-          activeKey={activeTab}
-          onChange={handleTabChange}
-          animated
-          centered // Align tab headings in the center
-        >
+        <Tabs activeKey={activeTab} onChange={handleTabChange} centered>
           <TabPane tab="Active Jobs" key="active">
-            <Row gutter={16} justify="center"> {/* Center-align the cards */}
+            <Row gutter={16} justify="center">
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 1" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 1" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 2" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 2" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 3" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 3" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
@@ -47,17 +42,17 @@ const JobsPage = () => {
           <TabPane tab="Posted Jobs" key="posted">
             <Row gutter={16} justify="center">
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 4" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 4" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 5" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 5" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 6" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 6" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
@@ -67,17 +62,17 @@ const JobsPage = () => {
           <TabPane tab="Completed Jobs" key="completed">
             <Row gutter={16} justify="center">
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 7" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 7" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 8" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 8" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
               <Col xs={24} sm={12} md={8}>
-                <Card title="Job Title 9" extra={<a href="#">Details</a>}>
+                <Card title="Job Title 9" extra={<Button type="primary">View Details</Button>}>
                   Job details go here.
                 </Card>
               </Col>
