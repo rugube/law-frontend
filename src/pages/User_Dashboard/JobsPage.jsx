74fe996507e3582d0c688edad3627ee579f2e5df
rugube <rrugube69@gmail.com>
2023-10-20 09:21:32 +0200

    center alignment

--- src/pages/User_Dashboard/JobsPage.jsx
+++ src/pages/User_Dashboard/JobsPage.jsx
@@ -17,10 +17,15 @@ const JobsPage = () => {
       <DashNavbar />
       <UserProfile />
       <div className="jobs-container">
-        <h1>Jobs</h1>
-        <Tabs activeKey={activeTab} onChange={handleTabChange} animated>
+        <h1 style={{ textAlign: 'center' }}>Jobs</h1>
+        <Tabs
+          activeKey={activeTab}
+          onChange={handleTabChange}
+          animated
+          centered // Align tab headings in the center
+        >
           <TabPane tab="Active Jobs" key="active">
-            <Row gutter={16}>
+            <Row gutter={16} justify="center"> {/* Center-align the cards */}
               <Col xs={24} sm={12} md={8}>
                 <Card title="Job Title 1" extra={<a href="#">Details</a>}>
                   Job details go here.
@@ -40,7 +45,7 @@ const JobsPage = () => {
           </TabPane>
 
           <TabPane tab="Posted Jobs" key="posted">
-            <Row gutter={16}>
+            <Row gutter={16} justify="center">
               <Col xs={24} sm={12} md={8}>
                 <Card title="Job Title 4" extra={<a href="#">Details</a>}>
                   Job details go here.
@@ -60,7 +65,7 @@ const JobsPage = () => {
           </TabPane>
 
           <TabPane tab="Completed Jobs" key="completed">
-            <Row gutter={16}>
+            <Row gutter={16} justify="center">
               <Col xs={24} sm={12} md={8}>
                 <Card title="Job Title 7" extra={<a href="#">Details</a>}>
                   Job details go here.
